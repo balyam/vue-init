ed059b7613d8f25d8a6b42ee892d4df95d169ac5
balyam <balyam@mail.ru>
2021-02-25 17:28:57 +0600

    fix error

--- src/App.vue
+++ src/App.vue
@@ -48,6 +48,7 @@
         </div>
         <button
         @click="add"
+
           type="button"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
@@ -246,4 +247,5 @@ export default {
   }
 };
 </script>
+
 
